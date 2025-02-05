8e124b4b5f6276f2494372f7cd947bcbdcd9e119
nakaari <78292028+nakaari@users.noreply.github.com>
2025-02-05 10:21:26 -0600

    goblin and flying eye

--- Goblin/goblin.gd
+++ Goblin/goblin.gd
@@ -1,28 +1,76 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+class_name Goblin
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const speed = 50
+var is_eye_chase: bool = false
 
+var health = 2
+var health_max = 2
+var health_min = 0
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
+var dead: bool = false
+var taking_damage: bool = false
+var damage_to_deal = 1
+var is_dealing_damage: bool = false
+
+var dir: Vector2
+const gravity = 900
+var knockback_force = 200
+var is_roaming: bool = true
+
+var goblin: CharacterBody2D
+var goblin_in_area = false
+
+func _process(delta):
+	if !is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		
+	
+	move(delta)
+	handle_animation()
 	move_and_slide()
+
+func move(delta):
+	if !dead:
+		if !is_eye_chase:
+			velocity += dir * speed * delta
+		#elif is_eye_chase and !taking_damage:
+			#var dir_to_player = position.direction_to(goblin.position) * speed
+			#velocity.x = dir_to_player.x
+		is_roaming = true
+	elif dead:
+		velocity.x = 0	
+
+func handle_animation():
+	var anim_sprite = $AnimatedSprite2D
+	if !dead and !taking_damage and !is_dealing_damage:
+		anim_sprite.play("run")
+		if dir.x == -1:
+			anim_sprite.flip_h = true
+		elif dir.x == 1:
+			anim_sprite.flip_h = false
+	elif !dead and taking_damage and !is_dealing_damage:
+		anim_sprite.play("take_hit")
+		await get_tree().create_timer(.8).timeout
+		taking_damage = false
+	elif dead and is_roaming:
+		is_roaming = false
+		anim_sprite.play("death")
+		await get_tree().create_timer(1.0).timeout
+		handle_death()
+		
+func handle_death():
+	self.queue_free()
+
+func _on_direction_timer_timeout():
+	$DirectionTimer.wait_time = choose([1.5,2.0,2.5])
+	if !is_eye_chase:
+		dir = choose([Vector2.RIGHT, Vector2.LEFT])
+		velocity.x = 0
+	
+func choose(array):
+	array.shuffle()
+	return array.front()
